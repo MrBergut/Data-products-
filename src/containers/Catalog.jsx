6297478bf7ccc18cb99a489ab6239ae2d2848173
MrBergut <torgovickijdana@gmail.com>
2024-04-17 19:20:42 +0300

    Added a paginator (not working yet) and buttons that limit the number of products (not working yet).

--- src/containers/Catalog.jsx
+++ src/containers/Catalog.jsx
@@ -1,9 +1,37 @@
 import Card from '../components/Card';
 import data from './ololo';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function Catalog(props) {
-    // const products = JSON.parse(data)
-        console.log(data)
-        return (data.products??[]).map(item=><Card {...item}/>)
+    const [products, setProducts] = useState([])
+    const [limit, setLimit] = useState(0)
+    const [skip, setSkip] = useState(0)
+    const [query, setQuery] = useState('')
+
+    const getProductsByQuery = useCallback(async () => {
+        const url = new URL('https://dummyjson.com/products/search')
+        query && url.searchParams.set('q', query)
+        const productsData = await fetch(url).then(res => res.json())
+        setProducts(productsData.products)
+    }, [query])
+
+    const getProductsList = useCallback(async () => {
+        const url = new URL('https://dummyjson.com/products')
+        limit && url.searchParams.set('limit', limit)
+        skip && url.searchParams.set('skip', skip)
+        const productsData = await fetch(url).then(res => res.json())
+        console.log('===ASDE===', productsData)
+        setProducts(productsData.products)
+    }, [limit, skip])
+
+    useEffect(() => {
+        getProductsList()
+    }, [])
+
+    useEffect(() => {
+        getProductsByQuery()
+    }, [query])
+
+    console.log(data)
+    return (data.products??[]).map(item=><Card {...item}/>)
 }
